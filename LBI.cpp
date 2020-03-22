82ae4e1ebd310b2d4ec03e0fb959ac1fe91f58a2
Compiled-Code <61854033+Compiled-Code@users.noreply.github.com>
2020-03-21 22:19:19 -0400

    Add files via upload

---
+++ LBI.cpp
@@ -0,0 +1,114 @@
+extern "C" {
+#include "Lua\lua.h"
+#include "Lua\lualib.h"
+#include "Lua\lauxlib.h"
+#include "Lua\luaconf.h"
+#include "Lua\llimits.h"
+}
+
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "LBI_D.h"
+
+#include <TlHelp32.h>
+#include <WinInet.h>
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "wininet.lib")
+
+using namespace std;
+
+namespace Internet {
+	string ReplaceAll(string subject, const string& search,
+		const string& replace) {
+		size_t pos = 0;
+		while ((pos = subject.find(search, pos)) != string::npos) {
+			subject.replace(pos, search.length(), replace);
+			pos += replace.length();
+		}
+		return subject;
+	}
+
+	string DownloadURL(string URL) {
+		HINTERNET interwebs = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, NULL);
+		HINTERNET urlFile;
+		string rtn;
+		if (interwebs) {
+			urlFile = InternetOpenUrlA(interwebs, URL.c_str(), NULL, NULL, NULL, NULL);
+			if (urlFile) {
+				char buffer[2000];
+				DWORD bytesRead;
+				do {
+					InternetReadFile(urlFile, buffer, 2000, &bytesRead);
+					rtn.append(buffer, bytesRead);
+					memset(buffer, 0, 2000);
+				} while (bytesRead);
+				InternetCloseHandle(interwebs);
+				InternetCloseHandle(urlFile);
+				string p = ReplaceAll(rtn, "|n", "\r\n");
+				return p;
+			}
+		}
+		InternetCloseHandle(interwebs);
+		string p = ReplaceAll(rtn, "|n", "\r\n");
+		return p;
+	}
+}
+
+int LBI() {
+	R::Bypass::b_console("Public LBI | CompiledCode");
+
+	const auto VFT = R::Off::ASLR(0x1C40458);
+	const auto ScriptContext = R::ASDF_Scanner::Scan(0x04, (char*)&VFT, (char*)"xxxx"); //source note: credit asdf for scanner. is not mine i do other method.
+	const auto rL = *(DWORD*)(ScriptContext + 56 * 0 + 172) - (DWORD)(ScriptContext + 56 * 0 + 172);
+	*(ULONG_PTR*)(*(ULONG_PTR*)(rL + 132) + 24) = 6;
+	
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+
+	while (true) {
+		std::string S;
+		std::getline(std::cin, S);
+		S = Internet::DownloadURL(S);
+		
+		luaL_loadbuffer(L, S.c_str(), S.length(), "LBI");
+		lua_setglobal(L, "__C__");
+
+		luaL_dostring(L, R"(
+			local bytecode = string.dump(__C__);
+			__SIZE__ = #bytecode
+			__BYTECODE__ = bytecode;
+		)");
+
+		lua_getglobal(L, "__BYTECODE__");
+		const auto lua_bytecode = lua_tostring(L, -1);
+
+		lua_getglobal(L, "__SIZE__");
+		const auto lua_bytecode_size = lua_tonumber(L, -1);
+
+		R::Off::pushlstring_roblox(rL, lua_bytecode, lua_bytecode_size);
+		R::Off::setfield_roblox(rL, LUA_GLOBALSINDEX, "SCRIPT_VALUE");
+
+		const auto LBI_LUAU = Internet::DownloadURL("https://github.com/Compiled-Code/roblox/blob/master/lbi.luau?raw=true");
+		R::Off::deserialize_roblox(rL, "LBI", LBI_LUAU.c_str(), LBI_LUAU.length());
+		R::Off::spawn_roblox(rL);
+	}
+
+	return 1;
+}
+
+BOOL APIENTRY DllMain(HMODULE v1, DWORD v2, void* v3)
+{
+	switch (v2)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(v1);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)LBI, NULL, NULL, NULL);
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	default: break;
+	}
+	return TRUE;
+}
---
+++ LBI_D.h
@@ -0,0 +1,139 @@
+namespace R {
+	namespace Bypass {
+		void b_console(const char* t) {
+			DWORD ConsoleProtect;
+			VirtualProtect((PVOID)&FreeConsole, 1, PAGE_EXECUTE_READWRITE, &ConsoleProtect);
+			*(BYTE*)(&FreeConsole) = 0xC3;
+
+			AllocConsole();
+			freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+			freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+			SetWindowPos(GetConsoleWindow(), HWND_TOPMOST, 0, 0, 0, 0, SWP_DRAWFRAME | SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW);
+			SetConsoleTitleA(t);
+		}
+
+		DWORD ret(DWORD addr)
+		{
+			BYTE* tAddr = (BYTE*)addr;
+			do
+			{
+				tAddr += 16;
+			} while (!(tAddr[0] == 0x55 && tAddr[1] == 0x8B && tAddr[2] == 0xEC));
+
+			DWORD funcSz = tAddr - (BYTE*)addr;
+
+			PVOID nFunc = VirtualAlloc(NULL, funcSz, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+			if (nFunc == NULL)
+				return addr;
+
+			memcpy(nFunc, (void*)addr, funcSz);
+
+			BYTE* pos = (BYTE*)nFunc;
+			BOOL valid = false;
+			do
+			{
+				if (pos[0] == 0x72 && pos[2] == 0xA1 && pos[7] == 0x8B) {
+					*(BYTE*)pos = 0xEB;
+
+					DWORD cByte = (DWORD)nFunc;
+					do
+					{
+						if (*(BYTE*)cByte == 0xE8)
+						{
+							DWORD oFuncPos = addr + (cByte - (DWORD)nFunc);
+							DWORD oFuncAddr = (oFuncPos + *(DWORD*)(oFuncPos + 1)) + 5;
+
+							if (oFuncAddr % 16 == 0)
+							{
+								DWORD relativeAddr = oFuncAddr - cByte - 5;
+								*(DWORD*)(cByte + 1) = relativeAddr;
+
+								cByte += 4;
+							}
+						}
+
+						cByte += 1;
+					} while (cByte - (DWORD)nFunc < funcSz);
+
+					valid = true;
+				}
+				pos += 1;
+			} while ((DWORD)pos < (DWORD)nFunc + funcSz);
+
+			if (!valid)
+			{
+				VirtualFree(nFunc, funcSz, MEM_RELEASE);
+				return addr;
+			}
+
+			return (DWORD)nFunc;
+		}
+	}
+
+	namespace Off {
+		DWORD ASLR(DWORD raw) {
+			return (raw - 0x400000 + (DWORD)GetModuleHandleA(0));
+		}
+
+		using t_d = int(__cdecl*)(DWORD v1, const char* v2, const char* v3, size_t v4);
+		t_d deserialize_roblox = (t_d)Bypass::ret(ASLR(0x8C5D40));
+
+		using nt_t = int(__cdecl*)(DWORD v1);
+		nt_t newthread_roblox = (nt_t)Bypass::ret(ASLR(0x7C9590));
+
+		using s_t = void(__cdecl*)(DWORD v1);
+		s_t spawn_roblox = (s_t)Bypass::ret(ASLR(0x728D20));
+
+		using ps_t = void(__cdecl*)(DWORD v1, const char* v2, int v3);
+		ps_t pushlstring_roblox = (ps_t)Bypass::ret(ASLR(0x7C9F20));
+
+		typedef void(__stdcall* g_t)(DWORD v1, int a2, const char* a3);
+		static g_t getfield_roblox = (g_t)Bypass::ret(ASLR(0x7C8B60));
+		
+		using sf_t = void(__stdcall*)(DWORD v1, int v2, const char* v3);
+		sf_t setfield_roblox = (sf_t)Bypass::ret(ASLR(0x7CAD20));
+	}
+
+	namespace ASDF_Scanner {
+		BOOL compare(const BYTE* location, const BYTE* aob, const char* mask) {
+			for (; *mask; ++aob, ++mask, ++location) {
+				__try {
+					if (*mask == 'x' && *location != *aob)
+						return 0;
+				}
+				__except (EXCEPTION_EXECUTE_HANDLER) {
+					return 0;
+				}
+			}
+			return 1;
+		}
+
+		DWORD find_Pattern(DWORD size, BYTE * pattern, char* mask,
+			BYTE protection = (PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY)) {
+			SYSTEM_INFO SI = { 0 };
+			GetSystemInfo(&SI);
+			DWORD start = (DWORD)SI.lpMinimumApplicationAddress;
+			DWORD end = (DWORD)SI.lpMaximumApplicationAddress;
+			MEMORY_BASIC_INFORMATION mbi;
+			while (start < end && VirtualQuery((void*)start, &mbi, sizeof(mbi))) {
+				// Make sure the memory is committed, matches our protection, and isn't PAGE_GUARD.
+				if ((mbi.State & MEM_COMMIT) && (mbi.Protect & protection) && !(mbi.Protect & PAGE_GUARD)) {
+					// Scan all the memory in the region.
+					for (DWORD i = (DWORD)mbi.BaseAddress; i < (DWORD)mbi.BaseAddress + mbi.RegionSize; ++i) {
+						if (compare((BYTE*)i, pattern, mask)) {
+							return i;
+						}
+					}
+				}
+				// Move onto the next region of memory.
+				start += mbi.RegionSize;
+			}
+			return 0;
+		}
+
+		int Scan(DWORD mode, char* content, char* mask) {
+			return find_Pattern(0x7FFFFFFF, (BYTE*)content, mask, mode);
+		}
+	}
+
+}
